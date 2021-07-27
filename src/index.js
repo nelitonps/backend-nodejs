412d044b27747d15527fbd6c2193499827419419
nelitonps <neliton.net@hotmail.com>
2021-07-27 11:53:51 -0300

    Concluindo a aplicação com NodeJS.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ const projects = [];
 //-------------------- Rotas -------------------------
 
 app.get('/projects', (request, response) => {
-   // const { title, owner } = request.query;
-   // console.log(title);
-   // console.log(owner);
+    const { title } = request.query;
+    const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-    return response.json(projects);
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
@@ -28,20 +29,33 @@ app.post('/projects', (request, response) => {
 
 app.put('/projects/:id', (request, response) => {
     const { id } = request.params;
-    console.log(id);
+    const { title, owner } = request.body;
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const projectIndex = projects.findIndex(project => project.id == id);
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3333, () => {
